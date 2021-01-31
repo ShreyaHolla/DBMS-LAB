e716bc668c816ce75ac50ecd76eeca4e977c2748
Shreya V Holla <shreyahere27@gmail.com>
2021-01-31 22:33:53 +0530

    Update create.sql

--- 2) Salesman Database/create.sql
+++ 2) Salesman Database/create.sql
@@ -1,11 +1,11 @@
 create table salesman(s_id varchar(20) primary key,s_name varchar(20),s_city varchar(20),commission varchar(20));
 
 create table customer(c_id varchar(20) primary key,c_name varchar(20),c_city varchar(20),grade integer, s_id varchar(20),
-foreign key(s_id) references salesman(s_id));
+foreign key(s_id) references salesman(s_id) on delete set null);
 
 create table orders(or_no varchar(20) primary key,pr_amt varchar(20),or_date date,c_id varchar(20),s_id varchar(20),
-foreign key(c_id) references customer(c_id),
-foreign key(s_id) references salesman(s_id));
+foreign key(c_id) references customer(c_id) on delete cascade,
+foreign key(s_id) references salesman(s_id) on delete cascade);
 
 insert into salesman values(1,'John','Bng','20%');
 insert into salesman values(2,'Sam','Bng','25%');
