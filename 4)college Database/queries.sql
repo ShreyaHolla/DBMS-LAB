65b81371eb6f136f20e4ae41c3a72dea62eb86b3
Shreya V Holla <shreyahere27@gmail.com>
2021-01-31 14:44:15 +0530

    Update queries.sql

--- 4)college Database/queries.sql
+++ 4)college Database/queries.sql
@@ -140,5 +140,90 @@ USN	SUBCODE	SSID	TEST1	TEST2	TEST3	FINALIA
 
             
 /*QUERIES*/
+/*1) List all the student details studying in fourth semester ‘C’ section. */
+select s.*,ss.sem,ss.sec
+from student s, semsec ss, class c
+where s.usn=c.usn
+and ss.ssid=c.ssid
+and ss.sem=4
+and ss.sec='C';
             
+
+
+USN	SNAME	ADDRESS	PHONE	GENDER	SEM	SEC
+1VA16CS003	NATASHA	BNG	7806734610	F	4	C
+
+/*2) Compute the total number of male and female students in each semester and in each section.*/
+select ss.sem,ss.sec,s.gender,count(s.gender) as count
+from student s,semsec ss,class c
+where s.usn=c.usn
+and ss.ssid=c.ssid
+group by ss.sem,ss.sec,s.gender
+order by sem;
             
+
+SEM	SEC	GENDER	COUNT
+4	A	M	1
+4	B	M	1
+4	C	F	1
+6	A	F	1
+6	B	M	1
+6	C	F	1
+8	A	F	1
+8	B	M	1
+8	C	F	1
+
+/*3) Create a view of Test1 marks of student USN ‘1VA15CS101’ in all Courses.*/
+create view test_marks as
+select subcode,test1 as marks
+from iamarks
+where usn='1VA15CS101';
+
+SELECT * FROM TEST_MARKS;
+            
+SUBCODE	MARKS
+15CS61	15
+15CS62	18
+15CS63	19
+
+/*4) Calculate the FinalIA (average of best two test marks) and update the corresponding table for all students.*/
+            
+ UPDATE IAMARKS
+SET FINALIA=case
+when(test1<=test2 and test1<=test3) then ceil((test2+test3)/2)
+when(test2<=test1 and test2<=test3) then ceil((test1+test3)/2)
+else ceil((test1+test2)/2)
+end;
+                                                                                                                     
+ 
+select * from iamarks;
+                        
+                                                                                                                     
+USN	SUBCODE	SSID	TEST1	TEST2	TEST3	FINALIA
+1VA15CS101	15CS61	CSE6A	15	16	17	17
+1VA15CS101	15CS62	CSE6A	18	19	20	20
+1VA15CS101	15CS63	CSE6A	19	19	20	20                                                                                                                     
+
+  /*5) Categorize students based on the following criterion: 
+ If FinalIA = 17 to 20 then CAT = ‘Outstanding’ 
+ If FinalIA = 12 to 16 then CAT = ‘Average’
+ If FinalIA< 12 then CAT = ‘Weak’
+Give these details only for 8/6th semester A, B, and C section students.*/            
+SELECT S.USN,S.SNAME,S.ADDRESS,S.PHONE,S.GENDER,IA.FINALIA,
+(CASE
+WHEN IA.FINALIA BETWEEN 17 AND 20 THEN 'OUTSTANDING'
+WHEN IA.FINALIA BETWEEN 12 AND 16 THEN 'AVERAGE'
+ELSE 'WEAK'
+END) AS CAT
+FROM STUDENT S, SEMSEC SS, IAMARKS IA, SUBJECT SUB
+WHERE S.USN = IA.USN AND
+SS.SSID = IA.SSID AND
+SUB.SUBCODE = IA.SUBCODE AND
+SUB.SEM = 6;            
+
+                                                                                                                     
+
+USN	SNAME	ADDRESS	PHONE	GENDER	FINALIA	CAT
+1VA15CS101	SOUNDARYA	BNG	7899142753	F	17	OUTSTANDING
+1VA15CS101	SOUNDARYA	BNG	7899142753	F	20	OUTSTANDING
+1VA15CS101	SOUNDARYA	BNG	7899142753	F	20	OUTSTANDING
